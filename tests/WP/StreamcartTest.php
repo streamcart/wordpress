432f4385ce964e1debcea7934873b68d4010065f
Mateus <mateusabissonho@gmail.com>
2024-08-14 14:58:08 -0300

    Make assertions compatible with old phpunit versions

--- tests/WP/StreamcartTest.php
+++ tests/WP/StreamcartTest.php
@@ -2,10 +2,6 @@
 
 namespace Streamcart\WP\Tests\WP;
 
-use function PHPUnit\Framework\assertFalse;
-use function PHPUnit\Framework\assertNotFalse;
-use function PHPUnit\Framework\assertNull;
-
 class StreamcartTest extends TestCase
 {
     public function setUp(): void
@@ -19,7 +15,7 @@ class StreamcartTest extends TestCase
      */
     public function test_is_user_data_consent_checked_without_user_consent(): void
     {
-        assertFalse(\Streamcart::instance()->is_user_data_consent_checked());
+        $this->assertFalse(\Streamcart::instance()->is_user_data_consent_checked());
     }
 
     /**
@@ -30,20 +26,20 @@ class StreamcartTest extends TestCase
     {
         $instance = \Streamcart::instance();
 
-        assertNotFalse( \has_action( 'plugins_loaded', [ $instance , 'load_textdomain' ] ) );
-        assertNotFalse( \has_action( 'admin_notices', [ $instance, 'add_admin_user_data_consent_notice' ] ) );
-        assertNotFalse( \has_action( 'after_plugin_row_meta', [ $instance, 'add_admin_user_data_consent_plugin_row_notice' ] ) );
-        assertFalse( \has_action( 'wp_head', [ $instance, 'add_script' ] ) );
+        $this->assertNotFalse( \has_action( 'plugins_loaded', [ $instance , 'load_textdomain' ] ) );
+        $this->assertNotFalse( \has_action( 'admin_notices', [ $instance, 'add_admin_user_data_consent_notice' ] ) );
+        $this->assertNotFalse( \has_action( 'after_plugin_row_meta', [ $instance, 'add_admin_user_data_consent_plugin_row_notice' ] ) );
+        $this->assertFalse( \has_action( 'wp_head', [ $instance, 'add_script' ] ) );
 
         $consentPage = $instance->getObject(\Streamcart_Consent::KEY);
 
-        assertNotFalse( \has_action( 'admin_menu', [ $consentPage , 'add_admin_menu' ] ) );
-        assertNotFalse( \has_action( 'admin_post_streamcart_consent_form', [ $consentPage , 'handle_consent_form' ] ) );
+        $this->assertNotFalse( \has_action( 'admin_menu', [ $consentPage , 'add_admin_menu' ] ) );
+        $this->assertNotFalse( \has_action( 'admin_post_streamcart_consent_form', [ $consentPage , 'handle_consent_form' ] ) );
 
         $adminSettingsPage = $instance->getObject(\Streamcart_Admin::KEY);
 
-        assertNotFalse( \has_action( 'admin_menu', [ $adminSettingsPage , 'add_admin_menu' ] ) );
-        assertNotFalse( \has_action( 'admin_init', [ $adminSettingsPage , 'settings_init' ] ) );
+        $this->assertNotFalse( \has_action( 'admin_menu', [ $adminSettingsPage , 'add_admin_menu' ] ) );
+        $this->assertNotFalse( \has_action( 'admin_init', [ $adminSettingsPage , 'settings_init' ] ) );
     }
 
     /**
@@ -69,22 +65,21 @@ class StreamcartTest extends TestCase
 
         $adminSettingsPage = \Streamcart::instance()->getObject(\Streamcart_Admin::KEY);
 
-        assertNotFalse( \has_action( 'admin_menu', [ $adminSettingsPage , 'add_admin_menu' ] ) );
-        assertNotFalse( \has_action( 'admin_init', [ $adminSettingsPage , 'settings_init' ] ) );
+        $this->assertNotFalse( \has_action( 'admin_menu', [ $adminSettingsPage , 'add_admin_menu' ] ) );
+        $this->assertNotFalse( \has_action( 'admin_init', [ $adminSettingsPage , 'settings_init' ] ) );
 
         \Streamcart::dispose();
-        $instance =
-            \Streamcart::instance();
+        $instance = \Streamcart::instance();
         $instance->includes();
 
-        assertNotFalse( \has_action( 'plugins_loaded', [ $instance , 'load_textdomain' ] ) );
-        assertFalse( \has_action( 'admin_notices', [ $instance, 'add_admin_user_data_consent_notice' ] ) );
-        assertFalse( \has_action( 'after_plugin_row_meta', [ $instance, 'add_admin_user_data_consent_plugin_row_notice' ] ) );
-        assertNotFalse( \has_action( 'wp_head', [ $instance, 'add_script' ] ) );
+        $this->assertNotFalse( \has_action( 'plugins_loaded', [ $instance , 'load_textdomain' ] ) );
+        $this->assertFalse( \has_action( 'admin_notices', [ $instance, 'add_admin_user_data_consent_notice' ] ) );
+        $this->assertFalse( \has_action( 'after_plugin_row_meta', [ $instance, 'add_admin_user_data_consent_plugin_row_notice' ] ) );
+        $this->assertNotFalse( \has_action( 'wp_head', [ $instance, 'add_script' ] ) );
 
         $wpdb->delete($wpdb->options, ['option_name' => 'streamcart_user_data_consent']);
 
-        assertNull($instance->getObject(\Streamcart_Consent::KEY));
+        $this->assertNull($instance->getObject(\Streamcart_Consent::KEY));
     }
 
 }
