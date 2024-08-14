73b278189f13e064dea49c4da17484bd41b0bf88
Mateus <mateusabissonho@gmail.com>
2024-08-14 14:47:13 -0300

    Remove unused code

--- tests/WP/bootstrap.php
+++ tests/WP/bootstrap.php
@@ -56,13 +56,4 @@ require_once $_tests_dir . 'includes/functions.php';
     3
 );
 
-/*
- * Bootstrap WordPress. This will also load the Composer autoload file, the PHPUnit Polyfills
- * and the custom autoloader for the TestCase and the mock object classes.
- */
 WPIntegration\bootstrap_it();
-
-if ( ! defined( 'WP_PLUGIN_DIR' ) || file_exists( WP_PLUGIN_DIR . '/streamcart/streamcart.php' ) === false ) {
-    echo PHP_EOL, 'ERROR: Please check whether the WP_PLUGIN_DIR environment variable is set and set to the correct value. The integration test suite won\'t be able to run without it.', PHP_EOL;
-    exit( 1 );
-}
