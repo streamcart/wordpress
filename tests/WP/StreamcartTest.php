7049ed5f37199e90d93a209d19d38b21c3fdf0c7
Mateus <mateusabissonho@gmail.com>
2024-08-14 10:41:08 -0300

    Test init hook with user consent checked

--- tests/WP/StreamcartTest.php
+++ tests/WP/StreamcartTest.php
@@ -7,11 +7,17 @@ use function PHPUnit\Framework\assertNotFalse;
 
 class StreamcartTest extends TestCase
 {
+
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
     /**
      * @covers \Streamcart::is_user_data_consent_checked
      * @return void
      */
-    public function test_is_user_data_consent_checked_without_user_consent()
+    public function test_is_user_data_consent_checked_without_user_consent(): void
     {
         assertFalse(\Streamcart::instance()->is_user_data_consent_checked());
     }
@@ -20,7 +26,8 @@ class StreamcartTest extends TestCase
      * @covers \Streamcart::init_hooks
      * @return void
      */
-    public function test_init_hooks_without_user_consent() {
+    public function test_init_hooks_without_user_consent(): void
+    {
         $instance = \Streamcart::instance();
 
         assertNotFalse( \has_action( 'plugins_loaded', [ $instance , 'load_textdomain' ] ) );
@@ -33,31 +40,32 @@ class StreamcartTest extends TestCase
      * @covers \Streamcart::init_hooks
      * @return void
      */
-    public function test_init_hooks_with_user_consent() {
+    public function test_init_hooks_with_user_consent(): void
+    {
         global $wpdb;
+
+        $insertResult = $wpdb->insert(
+            $wpdb->prefix . 'options',
+            [
+                'option_name' => 'streamcart_user_data_consent',
+                'option_value' => 'on',
+                'autoload' => 'auto'
+            ]
+        );
+
+        if(!$insertResult){
+            throw new \Exception("Could not insert into " . $wpdb->options);
+        }
 
         \Streamcart::dispose();
         $instance = \Streamcart::instance();
 
-        $wpdb->flush();
-
-//        if(!$wpdb->insert(
-//            $wpdb->prefix . 'options',
-//            [
-//                'option_name' => 'streamcart_user_data_consent',
-//                'option_value' => 'on',
-//                'autoload' => 'auto'
-//            ]
-//        )){
-//            throw new \Exception("Could not insert into " . $wpdb->options);
-//        }
-
         assertNotFalse( \has_action( 'plugins_loaded', [ $instance , 'load_textdomain' ] ) );
-//        assertFalse( \has_action( 'admin_notices', [ $instance, 'add_admin_user_data_consent_notice' ] ) );
-//        assertFalse( \has_action( 'after_plugin_row_meta', [ $instance, 'add_admin_user_data_consent_plugin_row_notice' ] ) );
+        assertFalse( \has_action( 'admin_notices', [ $instance, 'add_admin_user_data_consent_notice' ] ) );
+        assertFalse( \has_action( 'after_plugin_row_meta', [ $instance, 'add_admin_user_data_consent_plugin_row_notice' ] ) );
         assertNotFalse( \has_action( 'wp_head', [ $instance, 'add_script' ] ) );
 
-        //$wpdb->delete($wpdb->options, ['option_name' => 'streamcart_user_data_consent']);
+        $wpdb->delete($wpdb->options, ['option_name' => 'streamcart_user_data_consent']);
     }
 
 }
--- tests/WP/TestCase.php
+++ tests/WP/TestCase.php
@@ -5,4 +5,11 @@ namespace Streamcart\WP\Tests\WP;
 use Yoast\WPTestUtils\WPIntegration\TestCase as WPTestUtils_TestCase;
 
 abstract class TestCase extends WPTestUtils_TestCase
-{}
+{
+
+    public function set_up()
+    {
+        parent::set_up();
+    }
+
+}
